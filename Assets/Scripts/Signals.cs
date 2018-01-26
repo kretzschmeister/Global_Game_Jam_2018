0c9e431f0b3686f526b4f15db346a7c485181075
Sander Serrano Altena <s.serrano_altena@hotmail.com>
2018-01-26 22:51:10 +0100

    changed signal script

--- Assets/Scripts/Signals.cs
+++ Assets/Scripts/Signals.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Signals : MonoBehaviour {
+public class Signals : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    string state;
+    bool t = true;
+    public static float wayToGo;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ChangeState(t);
+        switch (state)
+        {
+            case "Happy":
+                wayToGo = 1;
+                break;
+            case "Sad":
+                wayToGo = -1;
+                break;
+            default:
+                break;
+        }
+
+
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Frienemy") {
+
+        }
+    }
+    public string ChangeState(bool activeState)
+    {
+        string state;
+        if (activeState)
+        {
+            state = "Happy";
+        }
+        else
+        {
+            state = "Sad";
+        }
+
+        return state;
+    }
 }
