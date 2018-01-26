fec4a9e99c0cb22581b0e9c7f2f4fba3fd3616d5
Sander Serrano Altena <s.serrano_altena@hotmail.com>
2018-01-26 21:34:15 +0100

    Added scripts

---
+++ Assets/Scripts/MovePingPong.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+/* This class is a simple class to move, 
+ * A rigidbody object over a set amount of time.
+ * it uses public booleans so you can adjust the 
+ * x and y pingpongs in the inspector
+ * also if it moves x or y or both
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePingPong : MonoBehaviour {
+
+	//TODO: Replace with other Rigid body
+	//Rigidbody2D platformRB;
+	public float moveTime = 1; 
+	bool moveBack;
+	private Vector3 startPos;
+	public bool movingX;
+	public bool movingY;
+
+	public float moveLength = 1;
+	// Use this for initialization
+	void Start () {
+
+		//TODO: replace with other rigidbody
+		//platformRB = GetComponent<Rigidbody2D> ();
+		startPos = transform.position;
+	}
+
+	// Dont mess with this code , go into the inspector.
+	void Update () {
+
+		if (movingX) {
+			transform.position = new Vector2 (PingPong (Time.time * moveTime, startPos.x, startPos.x + moveLength), transform.position.y);
+		}
+		if (movingY) {
+			transform.position = new Vector2 ( transform.position.x, PingPong (Time.time * moveTime, startPos.y, startPos.y + moveLength));
+		}
+	}
+	float PingPong ( float t, float minLength, float maxLength){
+		return Mathf.PingPong (t, maxLength - minLength) + minLength;
+	}
+}
---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitch : MonoBehaviour {
+
+
+	public string level ; 
+	// Use this for initialization
+
+	void OnTriggerEnter2D (Collider2D colider){
+		if (colider.gameObject.tag == "Player"){
+			Application.LoadLevel (level);
+		}
+	}
+}
