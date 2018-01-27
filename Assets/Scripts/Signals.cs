2e73225a5724216545e85d4a16b1e8b65eefc6a0
Sander Serrano Altena <s.serrano_altena@hotmail.com>
2018-01-27 01:25:17 +0100

    resolved conflicts

--- Assets/Scripts/Signals.cs
+++ Assets/Scripts/Signals.cs
@@ -26,7 +26,7 @@ public class Signals : MonoBehaviour
                 timer -= Time.deltaTime;
                 if (timer <= 0)
                 {
-                    FindObjectOfType<SwitchBar>().SpendPoints(spendPoints);
+                   // FindObjectOfType<SwitchBar>().SpendPoints(spendPoints);
                     timer = nTimer;
                 }
                 //Debug.Log("happy");
