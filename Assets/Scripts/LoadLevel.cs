c1d26148be8a2f861e97fb6e1c534695e300e3d5
Sander Serrano Altena <s.serrano_altena@hotmail.com>
2018-01-28 03:32:54 +0100

    0

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadLevel : MonoBehaviour {
-
+    public string scene1, scene2, scene3,scene4;
     // Use this for initialization
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Square") > 0)
+        {
+            LoadScene(scene1);
+            // this is an extra functional button
+            //TODO: Add the extra functionality ! Maybe a door ?
+        }
+        else if (Input.GetAxis("Triangle") > 0)
+        {
+            LoadScene(scene2);
+            // this is an extra functional button
+            //TODO: Add the extra functionality ! Maybe a door ?
+        }
+        else if (Input.GetAxis("Circle") > 0)
+        {
+            LoadScene(scene3);
+            // this is an extra functional button
+            //TODO: Add the extra functionality ! Maybe a door ?
+        }
+        else if (Input.GetAxis("X") > 0)
+        {
+            LoadScene(scene4);
+            // this is an extra functional button
+            //TODO: Add the extra functionality ! Maybe a door ?
+        }
+    }
     public void LoadScene(string destinationLevel)
     {
         SceneManager.LoadScene(destinationLevel);
